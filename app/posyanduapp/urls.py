6ec094c0e8340e3269bcfb169f0968847743099a
dean131 <deanabdillah@gmail.com>
2024-04-07 12:45:03 +0700

    Add adminapp to INSTALLED_APPS and admin template instalation

--- app/posyanduapp/urls.py
+++ app/posyanduapp/urls.py
@@ -16,10 +16,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('', include('adminapp.urls')),
+
     path('api/', include('account.api.urls')),
     path('api/', include('base.api.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
