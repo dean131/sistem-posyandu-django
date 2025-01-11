4fe2806e4f7c3b43a0fdc4100b67002fe3014831
Ilham Dean Abdillah <deanabdillah@gmail.com>
2025-01-11 11:22:54 +0700

    Update token lifetimes and enhance UI with icon buttons

--- app/posyanduapp/settings.py
+++ app/posyanduapp/settings.py
@@ -178,8 +178,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=150000),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=3000),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
     "UPDATE_LAST_LOGIN": False,
